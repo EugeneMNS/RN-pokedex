1d7624b51d98fa17f39e62d614cf00029e795687
EugeneMNS <evgen.str4@gmail.com>
2022-12-03 12:02:40 +0300

    add base screens

---
+++ src/screens/Details.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {Text, View} from "react-native";
+
+export const Details  = () => {
+    return (
+        <View>
+            <Text> Details screen</Text>
+        </View>
+    )
+}
