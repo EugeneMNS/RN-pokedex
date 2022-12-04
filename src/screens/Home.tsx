f0ada3dd94d5f1c992612be6906ea6269f609583
EugeneMNS <evgen.str4@gmail.com>
2022-12-04 10:16:27 +0300

    feat: list poke title

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,12 @@
 import React, {useEffect, useState} from "react";
-import {FlatList, ListRenderItem, StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import {FlatList, ImageBackground, ListRenderItem, StyleSheet, Text, TouchableOpacity, View} from "react-native";
 import {api, PokemonItem} from "../api/api";
 import {useAppNavigation} from "./types";
 import {useAppDispatch, useAppSelector} from "../store/store";
 import {getAllPokemon} from "../store/rootSlice";
 import {CardPokemon} from "../components/CardPokemon";
+
+import pokeballBackgroundImage from "../global/assets/Pokeball-bg-half.png";
 
 export const Home = () => {
 
@@ -21,22 +23,57 @@ export const Home = () => {
             <TouchableOpacity onPress={() => {
                 navigation.navigate("Details", {url: item.url})
             }}>
-                <View style={styles.item}>
+                {/*<View style={styles.item}>
                     <Text>{item.name}</Text>
-                </View>
+                </View>*/}
+                <CardPokemon />
             </TouchableOpacity>
         );
     }
 
     return (
-        <View style = {{paddingHorizontal:20}}>
-            {/*<FlatList data={allPokemon} renderItem={render} />*/}
-            <CardPokemon/>
+        <View style={styles.container}>
+            <ImageBackground style={styles.containerBackgroundImage}
+                             source={pokeballBackgroundImage}/>
+            <View style={styles.scrollView}>
+                <Text style={styles.title}>Pokedex</Text>
+                <Text style={styles.paragraph}>Find all Pokemon in one place</Text>
+                <View style={styles.content}>
+                    <FlatList data={allPokemon} renderItem={render}/>
+                </View>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#fff",
+        flex: 1,
+        position: "relative",
+    },
+    containerBackgroundImage: {
+        width: "100%",
+        height: 200,
+        position: "absolute",
+        top: 0,
+    },
+    scrollView: {
+        paddingHorizontal: 20,
+    },
+    content: {marginBottom: 50},
+    title: {
+        color: "#17171B",
+        fontSize: 32,
+        fontWeight: "bold",
+        marginTop: 80,
+    },
+    paragraph: {
+        color: "#747476",
+        fontSize: 16,
+        marginVertical: 10,
+        fontWeight: "400",
+    },
     item: {
         borderWidth: 1,
         borderRadius: 15,
